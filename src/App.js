ee96a8f1e08fe8fd4b1cdf9f4d3e50ce393d25db
Tommy Rosario <tomjasonrosario@gmail.com>
2020-11-28 14:21:07 -0500

    broken homepage into smaller components, finished styling menu item component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import './App.css';
-import Homepage from './homepage.component.jsx';
+import Homepage from './pages/homepage.component.jsx';
 
 function App() {
   return (
